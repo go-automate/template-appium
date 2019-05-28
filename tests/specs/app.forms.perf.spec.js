90492faf1908e21b5c8272f1ce338106373df6c8
Simon Stratton <simon.stratton@safebear.co.uk>
2019-05-28 16:09:04 +0100

    Adding some comments

--- tests/specs/app.forms.perf.spec.js
+++ tests/specs/app.forms.perf.spec.js
@@ -1,7 +1,5 @@
-import Gestures from '../helpers/Gestures';
 import TabBar from '../screenobjects/components/tab.bar';
 import FormScreen from '../screenobjects/forms.screen';
-import LoginScreen from '../screenobjects/login.screen';
 import fs from 'fs';
 
 describe('WebdriverIO and Appium, interacting with form elements,', () => {
@@ -12,24 +10,26 @@ describe('WebdriverIO and Appium, interacting with form elements,', () => {
     });
 
     it('should be able type in the input and validate the text', () => {
+        // Set up data for the Performance recording - need to set this up as a 'helper'
         let startArgs = {
             pid: 'current',
             profileName: 'Time Profiler',
             timeout: 60000
         };
 
+        // Stringify this - not sure this is needed
         let jsonStartArgs = JSON.stringify(startArgs);
 
-        console.log(jsonStartArgs);
-
+        // Set up data for stopping the performance recorder.
         let stopArgs = {
             profileName: 'Time Profiler',
         };
 
+        // Again, not sure this is needed
         let jsonStopArgs = JSON.stringify(stopArgs);
 
-        let perfResults;
 
+        // Start performance testing
         browser.execute('mobile: startPerfRecord', jsonStartArgs);
 
         const text = 'Hello, this is a demo app';
@@ -46,16 +46,18 @@ describe('WebdriverIO and Appium, interacting with form elements,', () => {
             driver.hideKeyboard();
         }
 
-        perfResults = browser.execute('mobile: stopPerfRecord', jsonStopArgs);
+        // Stop performance test and capture the results.
+        let perfResults = browser.execute('mobile: stopPerfRecord', jsonStopArgs);
 
         // Try decoding this.
-
         let buff = Buffer.from(perfResults, 'base64');
+        // Convert it into a byte array
         let perfDataDecoded = Uint8Array.from(buff);
 
-        // Create a file
+        // Create a file path
         const filePath = '/Users/standoubt/Documents/perf.zip';
 
+        // Save the byte array as a file, using the path above. It will be a Zip.
         fs.writeFile(filePath, perfDataDecoded, (err) => {
             if (err) console.log(err);
             console.log('Successfully Written to File');
